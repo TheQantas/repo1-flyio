00388d52bbaf3c4ff40a21e4e16dee50f508753f
CodyHager <chager004@gmail.com>
2025-02-23 19:15:27 -0700

    no more flask-security :skull

--- src/model/user.py
+++ src/model/user.py
@@ -1,3 +1,4 @@
+import uuid
 from peewee import *
 from flask_security import UserMixin, RoleMixin
 import ast
@@ -19,16 +20,36 @@ class Role(RoleMixin, user_db.Model):
 # N.B. order is important since db.Model also contains a get_id() -
 # we need the one from UserMixin.
 class User(UserMixin, user_db.Model):
-    email = TextField()
+    id = AutoField()
+    email = TextField(null=True)
     username = TextField()
     password = TextField()
     active = BooleanField(default=True)
-    fs_uniquifier = TextField(null=False)
-    confirmed_at = DateTimeField(null=True)
+    fs_uniquifier = TextField(default=lambda: str(uuid.uuid4()))
+    #confirmed_at = DateTimeField(null=True)
 
     @staticmethod
     def all() -> list['User']:
         return list(User.select())
+    
+    @staticmethod
+    def get_by_uid(u: str) -> 'User':
+        sel = User.select().where(User.fs_uniquifier == u)
+        return sel.first()
+    
+    @staticmethod
+    def get_by_username(uid: str) -> 'User':
+        sel = User.select().where(User.username == uid)
+        return sel.first()
+        
+    
+    @staticmethod
+    def add_user(username: str, hashed_password: str):
+        user = User.get_or_create(
+             username = username,
+             password = hashed_password
+        )
+        return user
 
 class UserRoles(user_db.Model):
     # Because peewee does not come with built-in many-to-many
