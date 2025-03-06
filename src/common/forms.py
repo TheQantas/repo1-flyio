f3a3a44ef023260cfa3682e47a54bdef03692094
dodds <matthewparkermt@gmail.com>
2025-03-06 14:39:30 -0700

    redid positioning + added icon & other change
    -"sign out" was moved to the very right properly
    -changed the way i did padding & positioning
    (see "index.css" mostly)
    -added an icon next to "sign out"
    -fixed the colors + underline for the button colors
    -removed button shadow (it was barely visible, didn't cover the entire bottom of the navbar, and looked weird)
    -replaced user.svg with the same image but without the blank space (so i could align the icon correctly)
    -changed text for the login form to accurately represent the design
    --(will figure out how to style this eventually)
    -removed the placeholder green background since search and new product button are in position

--- src/common/forms.py
+++ src/common/forms.py
@@ -2,6 +2,6 @@ from wtforms import StringField, PasswordField, SubmitField, validators
 from flask_wtf import FlaskForm
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[validators.input_required()])
+    username = StringField('Username Or Email Address', validators=[validators.input_required()])
     password  = PasswordField('Password', validators=[validators.input_required()])
     submit = SubmitField('Login')
